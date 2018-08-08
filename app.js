5370668e53a2bbef16bb5f0ef5482734a6308d48
Stéphane Adam Garnier <seedsodesign@gmail.com>
2018-08-08 12:30:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// stdin to be able to easily debug stuff that'd normally be sent from the Espruino UART
+var stdin = process.openStdin();
+
+
 // serial port part
 var sp = require('serialport');
 var Sp = sp.SerialPort;
@@ -15,19 +19,28 @@ var server = http.createServer(function(req, res) {
     });
 });
 
+// helper
+var sendInputToWs = function(data){
+  // directly send over ws to client ( we could also have parsed the data here & use xdotool to have sys-wide stuff )
+  if( theClient !== undefined && data.substr(0,3) === 'ws:' ){ // forward only websocket stuff, not all the stuff printed on serial console
+     socket.emit('message', data.substr(3) ); // forward any serial data to websockets as 'message' evt 
+  }
+}
+
 // serial port
 port.on('open', function(){
   console.log('serialport connected');
 });
 port.on('data', function(data){
   console.log('serialport data:' + data);
-    
-  // directly send over ws to client ( we could also have parsed the data here & use xdotool to have sys-wide stuff )
-  if( theClient !== undefined && data.substr(0,3) === 'ws:' ){ // forward only websocket stuff, not all the stuff printed on serial console
-     socket.emit('message', data.substr(3) ); // forward any serial data to websockets as 'message' evt 
-  }
+  sendInputToWs(data); // forward to websocket
 });
 
+// stdin
+stdin.addListener("data", function(data) {
+  console.log("you entered: [" + data.toString().trim() + "]");
+  sendInputToWs(data); // forward to websocket
+});
 
 // websockets
 var io = require('socket.io').listen(server);
