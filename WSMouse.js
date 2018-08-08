51e0e60c7cd54af5ee93e0d8b71093c51b56b115
Stéphane Adam Garnier <seedsodesign@gmail.com>
2018-08-08 07:37:56 +0200

    Create WSMouse.js

---
+++ WSMouse.js
@@ -0,0 +1,29 @@
+/* 
+   Original code ( USBMouse.js )
+   Copyright (c) 2015 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission.
+   
+   Below code ( WSMouse.js )
+   Copyright (c) 2018 Stephane Adam Garnier, SeedsDesign. MIT license.
+*/
+/* 
+
+```
+var wsmouse = require("WSMouse");
+
+setWatch(function() {
+  mouse.send(20,20,wsmouse.BUTTONS.NONE, 0); // X movement, Y movement, buttons pressed, scrollwheel mvmt
+}, BTN, {debounce:100,repeat:true, edge:"rising"});
+```
+
+*/
+
+exports.BUTTONS = {
+  NONE : 0,
+  LEFT : 1,
+  RIGHT : 2,
+  MIDDLE : 4
+};
+
+exports.send = function(x,y,b,s) {
+  console.log( JSON.stringify( { 'wsmouse.send': ([b&7,x,y,s] } ) ); // log a JSON obj to the serial to forward to websockets
+};
